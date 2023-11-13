d752f58757adf0cd57f30ec61b33516d5e283401
Cody Derbyshire <derbyshire@outlook.co.nz>
2023-11-13 18:16:46 +1300

    made web responsive, finished for now lmao

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -29,82 +29,84 @@ const ProductPage = () => {
             &larr; back
           </motion.a>
         </div>
-        <div className='product-spline-wrapper'>
-          <Spline
-            className='product-spline-model'
-            scene='https://prod.spline.design/YZAdC92tGORsOyN6/scene.splinecode'
-          />
-        </div>
-        <div className='product-info-wrapper'>
-          <div className='bold product-name-price'>
-            <p className='product-name'>Nora</p>
-            <p className='product-price'>$960.00</p>
+        <div className='web-wrapper'>
+          <div className='product-spline-wrapper'>
+            <Spline
+              className='product-spline-model'
+              scene='https://prod.spline.design/YZAdC92tGORsOyN6/scene.splinecode'
+            />
           </div>
-          <p className='product-desc'>
-            A timeless mid-century inspired side table combining style and
-            function. With an open shelf and a convenient drawer,
-            <span className='bold'> Nora</span> offers storage in a sleek and
-            elegant design. Elevate your living space with this versatile and
-            stylish addition.
-          </p>
-          <div className='product-details'>
-            <motion.a
-              className='bold'
-              whileHover={{
-                textDecoration: 'underline',
-                scale: 1.05,
-              }}
-              whileTap={{
-                scale: 0.95,
-              }}
-            >
-              Product Details ↗
-            </motion.a>
-            <p>
-              Available in <span className='bold'>Ash</span> &{' '}
-              <span className='bold'>Walnut</span>
+          <div className='product-info-wrapper'>
+            <div className='bold product-name-price'>
+              <p className='product-name'>Nora</p>
+              <p className='product-price'>$960.00</p>
+            </div>
+            <p className='product-desc'>
+              A timeless mid-century inspired side table combining style and
+              function. With an open shelf and a convenient drawer,
+              <span className='bold'> Nora</span> offers storage in a sleek and
+              elegant design. Elevate your living space with this versatile and
+              stylish addition.
             </p>
-          </div>
-          <div className='cart-wrapper'>
-            <motion.p
-              style={{
-                color: bgc,
-                backgroundColor: textcol,
-                filter: 'none',
-                border: `solid 0.1rem ${textcol}`,
-              }}
-              className='bold cart-btn'
-              whileHover={{
-                color: textcol,
-                backgroundColor: bgc,
-                filter: `drop-shadow(.35rem .35rem 0 ${textcol})`,
-                scale: 1.01,
-              }}
-              whileTap={{
-                scale: 0.95,
-              }}
-            >
-              add to cart
-            </motion.p>
-            <motion.svg
-              className='heart-btn'
-              xmlns='http://www.w3.org/2000/svg'
-              id='Layer_1'
-              data-name='Layer 1'
-              viewBox='0 0 24 24'
-              width='500'
-              height='500'
-              whileHover={{
-                scale: 1.1,
-              }}
-              whileTap={{
-                scale: 0.95,
-                fill: 'var(--tertiary-pink)',
-                backgroundColor: 'var(--dark)',
-              }}
-            >
-              <path d='M17.5.917a6.4,6.4,0,0,0-5.5,3.3A6.4,6.4,0,0,0,6.5.917,6.8,6.8,0,0,0,0,7.967c0,6.775,10.956,14.6,11.422,14.932l.578.409.578-.409C13.044,22.569,24,14.742,24,7.967A6.8,6.8,0,0,0,17.5.917Z' />
-            </motion.svg>
+            <div className='product-details'>
+              <motion.a
+                className='bold'
+                whileHover={{
+                  textDecoration: 'underline',
+                  scale: 1.05,
+                }}
+                whileTap={{
+                  scale: 0.95,
+                }}
+              >
+                Product Details ↗
+              </motion.a>
+              <p>
+                Available in <span className='bold'>Ash</span> &{' '}
+                <span className='bold'>Walnut</span>
+              </p>
+            </div>
+            <div className='cart-wrapper'>
+              <motion.p
+                style={{
+                  color: bgc,
+                  backgroundColor: textcol,
+                  filter: 'none',
+                  border: `solid 0.1rem ${textcol}`,
+                }}
+                className='bold cart-btn'
+                whileHover={{
+                  color: textcol,
+                  backgroundColor: bgc,
+                  filter: `drop-shadow(.35rem .35rem 0 ${textcol})`,
+                  scale: 1.01,
+                }}
+                whileTap={{
+                  scale: 0.95,
+                }}
+              >
+                add to cart
+              </motion.p>
+              <motion.svg
+                className='heart-btn'
+                xmlns='http://www.w3.org/2000/svg'
+                id='Layer_1'
+                data-name='Layer 1'
+                viewBox='0 0 24 24'
+                width='500'
+                height='500'
+                whileHover={{
+                  scale: 1.1,
+                }}
+                whileTap={{
+                  scale: 0.95,
+                  fill: 'var(--tertiary-pink)',
+                  backgroundColor: 'var(--dark)',
+                }}
+              >
+                <path d='M17.5.917a6.4,6.4,0,0,0-5.5,3.3A6.4,6.4,0,0,0,6.5.917,6.8,6.8,0,0,0,0,7.967c0,6.775,10.956,14.6,11.422,14.932l.578.409.578-.409C13.044,22.569,24,14.742,24,7.967A6.8,6.8,0,0,0,17.5.917Z' />
+              </motion.svg>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -6,76 +6,80 @@ import InfoBlock from './InfoBlock';
 const Shop = () => {
   return (
     <>
+      <TopBanner content={{ title: 'welcome!' }} />
       <div className='shop-wrapper'>
-        <TopBanner content={{ title: 'welcome!' }} />
-        <div className='shop-row'>
-          <InfoBlock
-            content={{
-              title: 'New Arrivals',
-              info: 'Discover the latest additions to our collection.',
-              button: 'explore new arrivals →',
-              bgc: 'var(--primary-dark-green)',
-              textcol: 'var(--tertiary-blue)',
-            }}
-          />
-          <div className='spline-wrapper'>
-            <Spline
-              className='spline bg-s-dg'
-              scene='https://prod.spline.design/zUrOMBHorzun8hob/scene.splinecode'
+        <div className='shop-left'>
+          <div className='shop-row'>
+            <InfoBlock
+              content={{
+                title: 'New Arrivals',
+                info: 'Discover the latest additions to our collection.',
+                button: 'explore new arrivals →',
+                bgc: 'var(--primary-dark-green)',
+                textcol: 'var(--tertiary-blue)',
+              }}
+            />
+            <div className='spline-wrapper bg-s-dg'>
+              <Spline
+                className='spline '
+                scene='https://prod.spline.design/zUrOMBHorzun8hob/scene.splinecode'
+              />
+            </div>
+          </div>
+          <div className='shop-row-flipped'>
+            <div className='spline-wrapper bg-s-b'>
+              <Spline
+                className='spline '
+                scene='https://prod.spline.design/y3BVmqSdqiVTmDHb/scene.splinecode'
+              />
+            </div>
+            <InfoBlock
+              content={{
+                title: 'Chairs',
+                info: 'Comfort and style meet sustainability.',
+                button: 'explore chairs →',
+                bgc: 'var(--primary-blue)',
+                textcol: 'var(--tertiary-pink)',
+              }}
             />
           </div>
         </div>
-        <div className='shop-row'>
-          <div className='spline-wrapper'>
-            <Spline
-              className='spline bg-s-b'
-              scene='https://prod.spline.design/y3BVmqSdqiVTmDHb/scene.splinecode'
+        <div className='shop-right'>
+          <div className='shop-row'>
+            <InfoBlock
+              content={{
+                title: 'Sofas',
+                info: 'Cozy and concious at the heart of your living room.',
+                button: 'explore sofas →',
+                bgc: 'var(--primary-light-green)',
+                textcol: 'var(--primary-blue)',
+              }}
+            />
+            <div className='spline-wrapper bg-s-lg'>
+              <Spline
+                className='spline'
+                scene='https://prod.spline.design/FDOprUU7S2muxjha/scene.splinecode'
+              />
+            </div>
+          </div>
+          <div className='shop-row-flipped margin-bottom'>
+            <div className='spline-wrapper bg-s-bg'>
+              <Spline
+                className='spline '
+                scene='https://prod.spline.design/McXzhbD4NOpUCv-O/scene.splinecode'
+              />
+            </div>
+            <InfoBlock
+              content={{
+                title: 'Storage',
+                info: 'Stylish and eco-conscious organization.',
+                button: 'explore storage →',
+                bgc: 'var(--primary-blue-green)',
+                textcol: 'var(--tertiary-orange)',
+                link: '/category',
+              }}
             />
           </div>
-          <InfoBlock
-            content={{
-              title: 'Chairs',
-              info: 'Comfort and style meet sustainability.',
-              button: 'explore chairs →',
-              bgc: 'var(--primary-blue)',
-              textcol: 'var(--tertiary-pink)',
-            }}
-          />
-        </div>
-        <div className='shop-row'>
-          <InfoBlock
-            content={{
-              title: 'Sofas',
-              info: 'Cozy and concious at the heart of your living room.',
-              button: 'explore sofas →',
-              bgc: 'var(--primary-light-green)',
-              textcol: 'var(--primary-blue)',
-            }}
-          />
-          <div className='spline-wrapper'>
-            <Spline
-              className='spline bg-s-lg'
-              scene='https://prod.spline.design/FDOprUU7S2muxjha/scene.splinecode'
-            />
-          </div>
-        </div>
-        <div className='shop-row margin-bottom'>
-          <div className='spline-wrapper'>
-            <Spline
-              className='spline bg-s-bg'
-              scene='https://prod.spline.design/McXzhbD4NOpUCv-O/scene.splinecode'
-            />
-          </div>
-          <InfoBlock
-            content={{
-              title: 'Storage',
-              info: 'Stylish and eco-conscious organization.',
-              button: 'explore storage →',
-              bgc: 'var(--primary-blue-green)',
-              textcol: 'var(--tertiary-orange)',
-              link: '/category',
-            }}
-          />
         </div>
       </div>
     </>
